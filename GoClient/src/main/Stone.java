b257287a32b59a954fe4ca6f69d30c0ebf2a27ef
romango <romango@rick.cotse.net>
2012-05-15 18:01:09 +0000

    checkpoint

---
+++ GoClient/src/main/Stone.java
@@ -0,0 +1,42 @@
+package main;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Stone extends ImageView
+{
+  private int stoneColor;
+  private BoardPosition boardPosition;
+  private boolean alive=true;
+  
+    public Stone(Image stone_image, int stoneColor, BoardPosition boardPosition)
+    {
+       this.boardPosition=boardPosition;
+       this.stoneColor=stoneColor;
+       setImage(stone_image);	
+       
+       
+       setX(boardPosition.getSceneX());
+       setY(boardPosition.getSceneY());
+    }
+    
+	public int getStoneColor() {
+		return stoneColor;
+	}
+	public void setStoneColor(int stoneColor) {
+		this.stoneColor = stoneColor;
+	}
+	public BoardPosition getBoardPosition() {
+		return boardPosition;
+	}
+	public void setBoardPosition(BoardPosition boardPosition) {
+		this.boardPosition = boardPosition;
+	}
+	public boolean isAlive() {
+		return alive;
+	}
+	public void setAlive(boolean alive) {
+		this.alive = alive;
+	}
+
+}
