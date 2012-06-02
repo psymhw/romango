bd2a1211f646db468a4376998fabdb151f1f8849
romango <romango@rick.cotse.net>
2012-06-02 18:21:34 +0000

    checkpoint

--- DgsPal/src/main/BoardMap.java
+++ DgsPal/src/main/BoardMap.java
@@ -25,11 +25,40 @@ public class BoardMap
 			if (identical==false) break;
 			if (this.moveMap[i][j]!=moveMap[i][j])
 			{
+			  //System.out.println("unequal at: "+i+"-"+j);	
 			  identical=false;
 			  break;
 			}
 		  }
 		}
 	  return identical;
+  }
+  
+  public String lastRow()
+  {
+	StringBuffer sb = new StringBuffer();
+	for(int i=0; i<19; i++)
+	{
+		sb.append(moveMap[i][18]);
+		sb.append(" ");
+	}
+	return sb.toString();
+  }
+  
+  
+  public void restore(int[][] moveMap)
+  {
+	  for(int j=0; j<19; j++)
+		{
+		  for(int i=0; i<19; i++)
+		  {
+			moveMap[i][j]=this.moveMap[i][j];
+		  }
+		}
+   }
+  
+  public int[][] get()
+  {
+	return this.moveMap;
   }
 }
