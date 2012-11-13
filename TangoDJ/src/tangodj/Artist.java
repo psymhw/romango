4df6f2695c1bb5d1b2c63e89ab077eb59550088d
rick <rick@romangoshoes.com>
2012-11-13 15:57:00 +0000

    checkpoint

---
+++ TangoDJ/src/tangodj/Artist.java
@@ -0,0 +1,13 @@
+package tangodj;
+
+public class Artist 
+{
+  public String firstName;
+  public String lastName;
+  
+  public Artist(String firstName, String lastName)
+  {
+	  this.firstName=firstName;
+	  this.lastName=lastName;
+  }
+}
