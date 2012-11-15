988d071e06c4f33da0be32b889112a8f27c827bd
rick <rick@romangoshoes.com>
2012-11-15 05:44:15 +0000

    checkpoint

--- TangoDJ/src/tangodj/InfoWindow.java
+++ TangoDJ/src/tangodj/InfoWindow.java
@@ -6,6 +6,7 @@ import java.util.Random;
 import javafx.geometry.Bounds;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.effect.DropShadow;
@@ -23,22 +24,12 @@ import test.Test;
 
 public class InfoWindow 
 {
- 
-  Random random = new Random();
-  public ArrayList<Font> fontList = new ArrayList<Font>();
   public ArrayList<FontMeta> fontMeta = new ArrayList<FontMeta>();
   Group root = new Group();
-  Artist lastArtist;
-  int lastFontNumber=0;
-  boolean test=false;
-  final int MAX_FONT_WIDTH=1100;
-  final int MAX_FONT_HEIGHT=500;
- 
   
   public InfoWindow(String artist, String title, boolean test)
   {
 	 this(artist, title); 
-	 this.test=test;
   }
   
   public InfoWindow(String artist, String title)
@@ -46,28 +37,16 @@ public class InfoWindow
     loadFonts();
 	 
 	Stage infoWindow = new Stage();
-	Artist currentArtist = massageArtist(artist);
-	Text artistText = getRandomEffect(artist);
-	 
-	lastArtist=currentArtist;
-	
-	Text trackNameText = new Text(title);
-    	
-    // TODO
-    // infoWindow.initStyle(StageStyle.TRANSPARENT);
-    	
     infoWindow.initModality(Modality.NONE);
-        
-    GridPane gp = getGridPane();
-    gp.add(artistText, 0, 0);
-    gp.add(trackNameText, 0, 1);
-        
-    final Rectangle r = new Rectangle(1200, 600);
+    
+    
+    Rectangle r = new Rectangle(1200,800);
     r.setFill(Color.DARKGRAY);
-        
+    
     root.getChildren().add(r);
-    root.getChildren().add(gp);
-    Scene scene = new Scene(root, 1200, 600);
+    update(artist, title);
+    
+    Scene scene = new Scene(root, 1200, 800);
     infoWindow.setScene(scene);
     infoWindow.show();
   }
@@ -83,132 +62,106 @@ public class InfoWindow
     return gridPane;
   }
   
+  
+  
   public void update(String artist, String title)
   {
-	int effectChoice=random.nextInt(1);
-	int fontChoice=0;
-	Artist currentArtist = massageArtist(artist);
-	if (lastArtist.lastName.equals(currentArtist.lastName))
-	  fontChoice=lastFontNumber;
-	else
-	  fontChoice= random.nextInt(10);
-	  
-	update(artist, title, fontChoice, effectChoice); 
-  }
-  
-  public void update(String artist, String title, int fontIndex, int effectIndex)
-  {
-  	Text artistText;
-  	Text titleText = new Text(title);
-  	
-  	
   	Artist currentArtist=massageArtist(artist);
-  	  	  	
-  	 // if (effectIndex==1)  artistText = getDropShadow(artist , fontList.get(fontIndex));
-  	//  else  artistText =  getDistantLight(artist, fontList.get(fontIndex));
-  	  
-  	
-  	  artistText =  getDistantLight(artist, fontList.get(fontIndex));
-
-  	  getFont(currentArtist.lastName, fontMeta.get(fontIndex));
+  	   	  
+  	 Text artistLastNameText =  currentArtist.getLastNameText();
+  	 Text artistFirstNameText =  currentArtist.getFirstNameText();
+   	 Text titleText = Artist.getDistantLight(title, currentArtist.firstNameFont);
   	  
   	 GridPane gp = getGridPane();
-  	 
-     GridPane.setHalignment(artistText, HPos.CENTER);
+  	 gp.setAlignment(Pos.CENTER);
+     gp.setPrefWidth(1190);
+    	 
+     gp.add(artistFirstNameText, 0, 0);
+     gp.add(artistLastNameText, 0, 1);
+     gp.add(titleText, 0, 2);
+     
+	 GridPane.setHalignment(artistFirstNameText, HPos.CENTER);
+     GridPane.setHalignment(artistLastNameText, HPos.CENTER);
      GridPane.setHalignment(titleText, HPos.CENTER);
      
-  	 lastArtist=currentArtist;
-  	 lastFontNumber=fontIndex;
-  	 
-     gp.add(artistText, 0, 0);
-     gp.add(new Text(title), 0, 1);
-  	
-    root.getChildren().remove(1);
+     
+  	if (root.getChildren().size()>1)  root.getChildren().remove(1);
   	root.getChildren().add(gp);
   }
   
-  private Text getRandomEffect(String txt)
-  {
-  	int choice=random.nextInt(1);
-  	if (choice==1) return getDropShadow(txt, fontList.get(random.nextInt(10)));
-  	else return getDistantLight(txt, fontList.get(random.nextInt(10)));
-  }
   
-  private Font getFont(String inStr, FontMeta fontMeta)
+  /*
+  
+  private Font getSizedFont(String inStr, FontMeta fontMeta)
   {
 	Font trialFont;  
 	Text trialText;
 	Bounds bounds;
+	int cycleCount=0;
+	String breakReason;
 	
 	int numberOfChars=inStr.length();
 	int fontSize=200;
-	if (numberOfChars>8) fontSize=160;
+	if (numberOfChars>8) fontSize=170;
 	if (numberOfChars>12) fontSize=140;
 	if (numberOfChars>16) fontSize=120;
 	
-	trialFont = Font.font(fontMeta.name, fontMeta.style, fontSize);
 	
-	trialText = new Text(inStr);
-	trialText.setFont(trialFont);
-	bounds=trialText.getBoundsInLocal();
+	while(true)
+	{
+	  trialFont = Font.font(fontMeta.name, fontMeta.style, fontSize);
+	
+	  trialText = new Text(inStr);
+	  trialText.setFont(trialFont);
+	  bounds=trialText.getBoundsInLocal();
+	  cycleCount++;
+	  if (cycleCount==20) { breakReason="cycle limit"; break; }
+	  if ((int)bounds.getWidth()>MAX_FONT_WIDTH) { breakReason="max width"; break; }
+
+	  if ((int)bounds.getHeight()>MAX_FONT_HEIGHT) { breakReason="max height"; break; }
+	  fontSize+=10;
+	}
 	
 	System.out.println("getFont() - str size: "+numberOfChars
-			+" fontSize: "+fontSize
-			+" width: "+(int)bounds.getWidth()
-			+" height: "+(int)bounds.getHeight()
+			+", fontSize: "+fontSize
+			+", width: "+(int)bounds.getWidth()
+			+", height: "+(int)bounds.getHeight()
+			+", reason: "+breakReason
+			+", cycles: "+cycleCount
 			);
-	
 	
 	
 	return trialFont;
 	
   }
   
-  private Font getSizedFont(String inStr, FontMeta fontMeta, int fontSize) 
-  {
-	Font aFont = Font.font(fontMeta.name, fontMeta.style, fontSize);
-	return null;
-}
+  */
 
 private void loadFonts()
   {
-    Font.loadFont(Test.class.getResource("/resources/fonts/blackrose.ttf").toExternalForm(), 10  );
+   
 	Font.loadFont(Test.class.getResource("/resources/fonts/Carousel.ttf").toExternalForm(), 10  );
 	Font.loadFont(Test.class.getResource("/resources/fonts/Anagram.ttf").toExternalForm(), 10  );
-	Font.loadFont(Test.class.getResource("/resources/fonts/blackjack.ttf").toExternalForm(), 10  );
+	
 	Font.loadFont(Test.class.getResource("/resources/fonts/Carrington.ttf").toExternalForm(), 10  );
 	Font.loadFont(Test.class.getResource("/resources/fonts/DEFTONE.ttf").toExternalForm(), 10  );
 	Font.loadFont(Test.class.getResource("/resources/fonts/EastMarket.ttf").toExternalForm(), 10  );
 	Font.loadFont(Test.class.getResource("/resources/fonts/england.ttf").toExternalForm(), 10  );
 	Font.loadFont(Test.class.getResource("/resources/fonts/FFF_Tusj.ttf").toExternalForm(), 10  );
-	Font.loadFont(Test.class.getResource("/resources/fonts/PermanentMarker.ttf").toExternalForm(), 10  );
-	Font.loadFont(Test.class.getResource("/resources/fonts/SEASRN.ttf").toExternalForm(), 10  );
+	
 
-	int fontSize=160;
-	fontList.add(Font.font("Black Rose", FontWeight.BOLD, fontSize));
-	fontList.add(Font.font("Carousel", FontWeight.BOLD, fontSize));
-	fontList.add(Font.font("Anagram", FontWeight.NORMAL, fontSize-20));
-	fontList.add(Font.font("BlackJack", FontWeight.BOLD, fontSize));
-	fontList.add(Font.font("Carrington", FontWeight.BOLD, fontSize));
-	fontList.add(Font.font("Deftone Stylus", FontWeight.BOLD, fontSize));
-	fontList.add(Font.font("EastMarket", FontWeight.BOLD, fontSize));
-	fontList.add(Font.font("England Hand DB", FontWeight.BOLD, fontSize));
-	fontList.add(Font.font("FFF Tusj", FontWeight.BOLD, fontSize-10));
-	fontList.add(Font.font("Permanent Marker", FontWeight.NORMAL, fontSize));
-	fontList.add(Font.font("SeasideResortNF", FontWeight.NORMAL, fontSize));
-	    
-	fontMeta.add(new FontMeta("Black Rose", FontWeight.BOLD));
+		
 	fontMeta.add(new FontMeta("Carousel", FontWeight.BOLD));
 	fontMeta.add(new FontMeta("Anagram", FontWeight.NORMAL));
-	fontMeta.add(new FontMeta("BlackJack", FontWeight.BOLD));
 	fontMeta.add(new FontMeta("Carrington", FontWeight.BOLD));
 	fontMeta.add(new FontMeta("Deftone Stylus", FontWeight.BOLD));
 	fontMeta.add(new FontMeta("EastMarket", FontWeight.BOLD));
 	fontMeta.add(new FontMeta("England Hand DB", FontWeight.BOLD));
 	fontMeta.add(new FontMeta("FFF Tusj", FontWeight.BOLD));
-	fontMeta.add(new FontMeta("Permanent Marker", FontWeight.NORMAL));
-	fontMeta.add(new FontMeta("SeasideResortNF", FontWeight.NORMAL));
+	
   }
+
+/*
   
 	private Text getDropShadow(String str, Font f)
 	{
@@ -225,40 +178,20 @@ private void loadFonts()
 	    return tx;
 	}
 	
-	private Text getDistantLight(String str, Font f)
-	{
-	  Light.Distant light = new Light.Distant();
-	  light.setAzimuth(-135.0f);
-	  light.setElevation(30.0f);
-
-	  Lighting l = new Lighting();
-	  l.setLight(light);
-	  l.setSurfaceScale(5.0f);
-
-	  final Text t = new Text();
-	  t.setText(str);
-	  t.setFill(Color.RED);
-	  t.setFont(f);
-	      
-	  //  t.setTextOrigin(VPos.TOP);
-
-	  t.setEffect(l);
-	
-	  return t;
-	}
+	*/
 
 	public Artist massageArtist(String inStr)
 	{
-	  if (inStr.toLowerCase().contains("biagi"))       return new Artist("Rudolfo", "Biagi");
-	  if (inStr.toLowerCase().contains("calo"))        return new Artist("Miguel", "Calo");
-	  if (inStr.toLowerCase().contains("canaro"))      return new Artist("Francisco", "Canaro");
-	  if (inStr.toLowerCase().contains("rodriguez"))   return new Artist("Enrique", "Rodriguez");
-	  if (inStr.toLowerCase().contains("d'arienzo"))   return new Artist("Juan", "D'Arienzo");
-	  if (inStr.toLowerCase().contains("de angelise")) return new Artist("Alfredo", "De Angelise");
-	  if (inStr.toLowerCase().contains("di sarli"))    return new Artist("Caslos", "Di Sarli");
+	  if (inStr.toLowerCase().contains("biagi"))       return new Artist("Rudolfo", "Biagi", new FontMeta("Carousel", FontWeight.BOLD), 300);
+	  if (inStr.toLowerCase().contains("calo"))        return new Artist("Miguel", "Calo", new FontMeta("Anagram", FontWeight.NORMAL), 300);
+	  if (inStr.toLowerCase().contains("canaro"))      return new Artist("Francisco", "Canaro", new FontMeta("Carrington", FontWeight.BOLD), 300);
+	  if (inStr.toLowerCase().contains("rodriguez"))   return new Artist("Enrique", "Rodriguez", new FontMeta("Deftone Stylus", FontWeight.BOLD), 300);
+	  if (inStr.toLowerCase().contains("d'arienzo"))   return new Artist("Juan", "D'Arienzo", new FontMeta("EastMarket", FontWeight.BOLD), 250);
+	  if (inStr.toLowerCase().contains("de angelise")) return new Artist("Alfredo", "De Angelise", new FontMeta("England Hand DB", FontWeight.BOLD), 250);
+	  if (inStr.toLowerCase().contains("di sarli"))    return new Artist("Carlos", "Di Sarli", new FontMeta("FFF Tusj", FontWeight.BOLD), 300);
 	  
 	  
-	  return new Artist("", inStr);
+	  return new Artist("", inStr, new FontMeta("Carousel", FontWeight.BOLD), 300);
 	}
 	
 }
