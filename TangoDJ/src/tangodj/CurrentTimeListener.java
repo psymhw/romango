e9d97fdd6c02f4d3c91a999f2b2180744ea6b3c9
rick@romangoshoes.com <rick@romangoshoes.com@4359a801-f2b7-23da-441c-843f2933f397>
2013-02-01 17:35:27 +0000

    checkpoint

--- TangoDJ/src/tangodj/CurrentTimeListener.java
+++ TangoDJ/src/tangodj/CurrentTimeListener.java
@@ -15,25 +15,26 @@ class CurrentTimeListener implements InvalidationListener
 	MediaPlayer player;
 	Label currentTimeLabel;
 	ProgressBar progress;
-	Duration duration;
+	
 	  @Override
 	  public void invalidated(Observable observable) {
 	    Platform.runLater(new Runnable() {
 	      @Override
 	      public void run() {
 	        final Duration currentTime = player.getCurrentTime();
-	        currentTimeLabel.setText(formatDuration(duration.toMillis()-currentTime.toMillis()));
+	        final Duration trackLength = player.getTotalDuration();
+	        currentTimeLabel.setText(formatDuration(trackLength.toMillis()-currentTime.toMillis()));
 	        progress.setProgress(1.0 * player.getCurrentTime().toMillis() / player.getTotalDuration().toMillis());
 	      }
 	    });
 	  }
 	  
-	  public CurrentTimeListener(MediaPlayer player, Label currentTimeLabel, ProgressBar progress, Duration duration)
+	  public CurrentTimeListener(MediaPlayer player, Label currentTimeLabel, ProgressBar progress)
 	  {
 		 this.player=player; 
 		 this.currentTimeLabel=currentTimeLabel;
 		 this.progress=progress;
-		 this.duration=duration;
+		 
 	  }
 	  
 	  private String formatDuration(double time) {
