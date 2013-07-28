8a9074e4b4d8f317164267f652584e564d141c84
rick@romangoshoes.com <rick@romangoshoes.com@4359a801-f2b7-23da-441c-843f2933f397>
2013-07-28 23:01:08 +0000

    checkpoint

---
+++ TangoDJ/src/tangodj2/Playlist.java
@@ -0,0 +1,21 @@
+package tangodj2;
+
+import java.sql.Date;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class Playlist 
+{
+	private final int id=0;
+	private final SimpleStringProperty name;
+	private final SimpleStringProperty location;
+	private final Date date=null;
+	
+	public Playlist(String name, String location)
+	{
+	  this.name=new SimpleStringProperty(name);
+	  this.location=new SimpleStringProperty(location);
+	}
+	
+}
---
+++ TangoDJ/src/tangodj2/SharedValues.java
@@ -0,0 +1,16 @@
+package tangodj2;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class SharedValues 
+{
+  public static SimpleStringProperty title = new SimpleStringProperty();
+  public static SimpleStringProperty pathHash = new SimpleStringProperty();
+  public static SimpleIntegerProperty playlistTrackAdd = new SimpleIntegerProperty(0);
+
+  
+  
+}
+
+
