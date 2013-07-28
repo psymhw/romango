508439f81fb9b5b59811cb53462bb17e41316fa0
rick <rick@romangoshoes.com>
2013-07-28 23:01:08 +0000

    checkpoint

---
+++ TangoDJ/src/tangodj2/CreateDatabase.java
@@ -0,0 +1,56 @@
+package tangodj2;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class CreateDatabase 
+{
+	private boolean exists=false;
+	static Connection connection;
+	public static final String DRIVER ="org.apache.derby.jdbc.EmbeddedDriver";
+	public static final String JDBC_URL ="jdbc:derby:tango_db;create=true";
+
+	
+	public CreateDatabase() throws ClassNotFoundException, SQLException, IOException
+	{
+	  File test = new File("tango_db");
+	  if (test!=null)
+	  {
+		if (test.exists()) exists=true;
+	  }
+	  
+	  if (exists) System.out.println("database found");
+	  else
+	  {
+		Class.forName(DRIVER);
+		 connection = DriverManager.getConnection(JDBC_URL);
+	    connection.createStatement().execute("create table tracks(" +
+	    		"id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), " +
+	    		"path varchar(300), " +
+	    		"pathHash varchar(32), " +
+	    		"title varchar(100), " +
+	    		"artist varchar(40), " +
+	    		"album varchar(100), " +
+	    		"genre varchar(40), " +
+	    		"comment varchar(100) " +
+	    		")");
+	    
+	    connection.createStatement().execute("CREATE UNIQUE INDEX pathInd ON TRACKS(PATHHASH)");
+	    connection.close();
+	    System.out.println("database initiallized");
+	  }
+	}
+	
+	public static void main(String[] args) 
+	{
+	  try
+	  {
+		 
+	    new CreateDatabase();
+	  } catch (Exception e) { e.printStackTrace(); }
+	}
+
+}
---
+++ TangoDJ/src/tangodj2/GenericCellFactory.java
@@ -0,0 +1,38 @@
+package tangodj2;
+
+import javafx.event.EventHandler;
+import javafx.event.EventType;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.input.MouseEvent;
+import javafx.util.Callback;
+
+public class GenericCellFactory implements Callback<TableColumn,TableCell> {
+    
+    
+    EventHandler click;
+    
+    
+    public GenericCellFactory(EventHandler click) {
+        
+        this.click = click;
+    }
+
+    public TableCell call(TableColumn p) {
+    	TableCell cell=cell = new TableCell() 
+         {
+            @Override 
+            protected void updateItem(Object item, boolean empty) {
+                 // calling super here is very important - don't skip this!
+                 super.updateItem(item, empty);
+                 if(item != null) {
+                     setText(item.toString());
+                 }
+            }
+       };
+      
+	   cell.setOnMouseClicked(click);     
+       return cell;
+    }
+}
