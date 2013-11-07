fcff14ecf5290e679bf968c948680ed5ce09b000
rick@romangoshoes.com <rick@romangoshoes.com@4359a801-f2b7-23da-441c-843f2933f397>
2013-11-07 19:11:36 +0000

    checkpoint

--- TangoDJ/src/tangodj2/Preferences.java
+++ TangoDJ/src/tangodj2/Preferences.java
@@ -6,15 +6,5 @@ public class Preferences
   public String cleanupFolder="c:";
   public int currentPlaylist=1;
   
-  public boolean albumCol=true;
-  public boolean yearCol=true;
-  public boolean genreCol=true;
-  public boolean styleCol=true;
-  public boolean lengthCol=true;
-  public boolean ratingCol=true;
-  public boolean singerCol=true;
-  public boolean adjectivesCol=true;
-  public boolean commentCol=true;
   
-  public double albumColWidth=110;
 }
