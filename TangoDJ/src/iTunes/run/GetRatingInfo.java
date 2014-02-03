64c6458e182e635d8bba0248cd76a1b37ff74e7c
rick@romangoshoes.com <rick@romangoshoes.com@4359a801-f2b7-23da-441c-843f2933f397>
2014-02-03 00:12:37 +0000

    checkpoint

---
+++ TangoDJ/src/iTunes/run/GetRatingInfo.java
@@ -0,0 +1,93 @@
+package iTunes.run;
+import iTunes.data.PlaylistData;
+import iTunes.data.TrackData;
+import iTunes.data.iTunesData;
+import iTunes.data.iTunesParser;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DecimalFormat;
+
+import javax.swing.JFrame;
+
+import org.apache.commons.codec.net.URLCodec;
+
+
+public class GetRatingInfo extends JFrame
+{
+  /*
+   * by Rick Roman
+   * Parts by Robbie Hanson http://www.robbiehanson.com/iTunesJava.html
+   * and Paul Grebenc http://jid3.blinkenlights.org/
+   */
+  private static Hasher hasher = new Hasher();
+  public GetRatingInfo()
+  {
+    
+    PlaylistData pd=null;
+    TrackData td = null;
+    String path=null;
+    String destFileName;
+    DecimalFormat nf = new DecimalFormat("00");
+    
+    String playlist="";
+    iTunesData data = new iTunesData();
+    iTunesParser parser = new iTunesParser(data);
+    parser.parseFile();
+    String pathHash;
+    BufferedWriter out=null;
+    File outFile = new File("loader_error.txt");
+    if (outFile.exists()) outFile.delete();
+    FileWriter fstream;
+    try
+    {
+      fstream = new FileWriter("ratings.txt");
+      out = new BufferedWriter(fstream);
+    } catch (IOException e)
+    {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+   
+    File file;
+    URLCodec ucd = new URLCodec();
+    
+    try
+    {
+      for(int i=0; i<data.tracks.length; i++)
+      { 
+        td =  data.tracks[i];
+        
+        
+        file = new File(ucd.decode(td.path.substring(17)));
+       // if (file.exists()) System.out.println("exists");
+       // else System.out.println("not exists");
+        
+        pathHash = hasher.getMd5Hash(file.getPath().toString().getBytes());
+        if (td.rating>0)
+        {  
+          out.write(pathHash+","+td.rating);
+          out.newLine();
+        }
+        System.out.println(pathHash+","+td.rating);
+        //if (i>200) break;
+      }
+      
+      out.close();
+    } catch (Exception e) { e.printStackTrace(); }
+  }
+
+  
+  public static void main(String args[])
+  {
+    
+    
+    System.out.println();
+       
+     
+    new GetRatingInfo();
+  }
+
+}
