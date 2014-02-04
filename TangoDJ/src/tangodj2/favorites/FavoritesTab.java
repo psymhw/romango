1960dd19afcf379cc293c4bdb5b8847458a22b82
rick@romangoshoes.com <rick@romangoshoes.com@4359a801-f2b7-23da-441c-843f2933f397>
2014-02-04 04:07:54 +0000

    checkpoint

--- TangoDJ/src/tangodj2/favorites/FavoritesTab.java
+++ TangoDJ/src/tangodj2/favorites/FavoritesTab.java
@@ -6,6 +6,7 @@ import tangodj2.TangoDJ2;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.SplitPane;
 import javafx.scene.control.Tab;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -37,6 +38,18 @@ public class FavoritesTab extends Tab
     vbox.getChildren().add(listsComboBox);
     vbox.getChildren().add(new Text("BOTTOM"));
     
+    SplitPane sp = new SplitPane();
+    sp.setStyle("-fx-background-color: plum;");
+    
+    
+    sp.getItems().addAll(listsComboBox, new Text("RIGHT"));
+    sp.setDividerPositions(0.5f);
+    
+    setContent(sp);
+
+    
+    
+    
     ListHeaderDb lhdb;
     Iterator<ListHeaderDb> it = TangoDJ2.favoritesList.iterator();
     while(it.hasNext())
@@ -46,7 +59,6 @@ public class FavoritesTab extends Tab
      // System.out.println(lhdb.getName());
     }
     
-    setContent(vbox);
   }
   
 }
