87d937595fa45472578a80e21906d14a87ce31fa
rick <rick@romangoshoes.com>
2014-02-04 03:48:34 +0000

    checkpoint

--- TangoDJ/src/tangodj2/favorites/FavoritesTab.java
+++ TangoDJ/src/tangodj2/favorites/FavoritesTab.java
@@ -1,7 +1,14 @@
 package tangodj2.favorites;
 
+import java.util.Iterator;
+
+import tangodj2.TangoDJ2;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Tab;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
 public class FavoritesTab extends Tab
@@ -11,11 +18,35 @@ public class FavoritesTab extends Tab
   {
     this.setText("Favorites");
     
-    HBox hbox = new HBox();
-    hbox.getChildren().add(new Text("TOP"));
-    hbox.getChildren().add(new Text("BOTTOM"));
+    final ComboBox listsComboBox = new ComboBox();
     
-    setContent(hbox);
+    
+    listsComboBox.getSelectionModel().selectedItemProperty().addListener
+    (
+      new ChangeListener<String>() 
+      {
+        public void changed(ObservableValue<? extends String> ov, String old_val, String new_val) 
+        { 
+          System.out.println("favorites list changed");
+        } 
+      });
+
+   // listsComboBox.setValue(trackDb.style);
+    
+    VBox vbox = new VBox();
+    vbox.getChildren().add(listsComboBox);
+    vbox.getChildren().add(new Text("BOTTOM"));
+    
+    ListHeaderDb lhdb;
+    Iterator<ListHeaderDb> it = TangoDJ2.favoritesList.iterator();
+    while(it.hasNext())
+    {
+      lhdb=it.next();
+      listsComboBox.getItems().add(lhdb.getName());
+     // System.out.println(lhdb.getName());
+    }
+    
+    setContent(vbox);
   }
   
 }
